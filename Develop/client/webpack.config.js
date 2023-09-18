3611c1678f19a346bc5edf118edccc84724e099a
Quincy Maynor <quincymaynor@gmail.com>
2023-09-18 18:00:59 -0500

    initial webpack.config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,7 +18,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+      new WebpackPwaManifest({
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Keep track of contacts.',
+        background_color: "#31a9e1",
+        theme_color: "#31a9e1",
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
